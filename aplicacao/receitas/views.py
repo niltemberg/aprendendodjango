3cc81963dd264bc99f2ff15c586885bb5f7e393c
Niltemberg <niltemberg@gmail.com>
2022-04-10 14:03:46 -0300

    adicionando paginas estáticas

--- aplicacao/receitas/views.py
+++ aplicacao/receitas/views.py
@@ -1,5 +1,8 @@
+import re
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1> Receitas </h1>')
+    return render(request, 'index.html')
+
+def receita(request):
+    return render(request, 'receita.html')
