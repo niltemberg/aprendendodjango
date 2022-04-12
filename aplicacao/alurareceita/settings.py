b5ef652a2b48645d69b4dcd0374d92075b7c6b8b
Niltemberg <niltemberg@gmail.com>
2022-04-12 00:09:05 -0300

    Filtros e Categorias

--- aplicacao/alurareceita/settings.py
+++ aplicacao/alurareceita/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pessoas',
     'receitas',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -125,6 +126,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'alurareceita/static')
 ]
+
+#Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- aplicacao/receitas/views.py
+++ aplicacao/receitas/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, render, get_list_or_404
 from .models import Receita
 
 def index(request):
-    receitas = Receita.objects.all()
+    receitas = Receita.objects.order_by('-publicada').filter(publicada=True)
     dados = {
         'receitas': receitas
     }
